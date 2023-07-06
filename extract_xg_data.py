31120f5ab9ed59b06dbc698cff595c30181c1792
prof401 <gerald.scheetz@april1.net>
2023-07-05 21:33:36 -0600

    basic extract of shot

--- extract_xg_data.py
+++ extract_xg_data.py
@@ -1,10 +1,29 @@
 # Get the database using the method we defined in pymongo_test_insert file
 from pymongo_get_database import get_database
-dbname = get_database()
 
+
+def shots(event: dict):
+    attr: list = event['tagAttributes']
+    result = ""
+    x = 0
+    y = 0
+    for item in attr:
+        match item['name']:
+            case 'Result':
+                result = item['value']
+            case 'Field Location':
+                x = item['value']['x']
+                y = item['value']['y']
+    ret_value = "Shot," + result + "," + str(x) + "," + str(y)
+    return ret_value
+
+
+dbname = get_database()
 
 # Retrieve a collection named "user_1_items" from database
 collection_name = dbname["vidswap"]
+
+extract_dict = {'Shot': shots}
 
 games = collection_name.find()
 for game in games:
@@ -13,6 +32,6 @@ for game in games:
     # This does not give a very readable output
     print(playlist['name'] + ' on ' + playlist['date'])
     for event in game['tagEvents']:
-        event_id = event['id']
-        print(event['tagResource']['name'])
-        attr = event['tagAttributes']
+        event_name = event['tagResource']['name']
+        if (event_name in extract_dict.keys()):
+            print(str(game_id) + "," + extract_dict[event_name](event))
